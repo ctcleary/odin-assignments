4e92cc972eef5c62f9f2d09d1c017a2563158626
Connor Thomas Cleary <connorthomascleary@gmail.com>
2024-12-24 06:56:57 -0500

    Dropdown menu assignment complete.

--- dropdown-assignment/src/index.js
+++ dropdown-assignment/src/index.js
@@ -1,4 +1,22 @@
 import "./styles.css";
-import { greeting } from "./greeting.js"
 
-console.log(greeting);
+const hamburgerButton = document.getElementById('hamburger-button');
+const dropdownMenu = document.getElementById('dropdown');
+
+hamburgerButton.addEventListener('click', () => {
+    if (dropdownMenu.classList.contains('visible')) {
+        dropdownMenu.classList.remove('visible');
+        hamburgerButton.innerHTML = '&middot; &middot; &middot;';
+    } else {
+        dropdownMenu.classList.add('visible');
+        hamburgerButton.innerHTML = '&middot; X &middot;';
+    }
+});
+
+const dropdownLinks = document.querySelectorAll('.dropdown-item a');
+
+dropdownLinks.forEach((el) => {
+    el.addEventListener('click', () => {
+        dropdownMenu.classList.remove('visible');
+    })
+})
