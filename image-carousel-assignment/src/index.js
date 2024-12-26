694f02816ee720595a7daca49b092c371bf8189c
Connor Thomas Cleary <connorthomascleary@gmail.com>
2024-12-26 06:09:56 -0500

    Carousel works, code needs serious cleanup.

--- image-carousel-assignment/src/index.js
+++ image-carousel-assignment/src/index.js
@@ -1,8 +1,10 @@
 import "./styles.css";
 
 const imgAssetPath = './img/';
-
+const imgWidthRem = 36;
 const imagesWide = document.getElementById('images-wide');
+
+// appendImageChild(10);
 
 function appendImageChild(picNum) {
     const containerDiv = document.createElement('div');
@@ -11,6 +13,7 @@ function appendImageChild(picNum) {
     const img = document.createElement('img');
     img.classList.add('carousel-image');
     img.src = `${imgAssetPath}gordon${picNum}.jpg`;
+    img.dataset.picNumber = picNum;
 
     containerDiv.appendChild(img);
     imagesWide.appendChild(containerDiv);
@@ -19,4 +22,50 @@ function appendImageChild(picNum) {
 for (let i = 1; i <= 10; i++) {
     appendImageChild(i);
 }
-appendImageChild(1);
+// appendImageChild(1);
+
+imagesWide.style = 'left:0rem;';
+
+const leftArrow = document.getElementById('arrow-left');
+const rightArrow = document.getElementById('arrow-right');
+
+const carousel = (function() {
+
+})();
+
+function changeSlideRight() {
+    const currLeftStr = imagesWide.style.left;
+    const currLeft = parseInt(currLeftStr);
+    console.log('currLeft', currLeft);
+
+    const newLeft = currLeft - imgWidthRem;
+    if (newLeft === (-36 * 10)) {
+        imagesWide.style = `left: 0rem;`
+    } else {
+        imagesWide.style = `left: ${newLeft}rem;`;
+    }
+}
+function changeSlideLeft() {
+    const currLeftStr = imagesWide.style.left;
+    const currLeft = parseInt(currLeftStr);
+    console.log('currLeft', currLeft);
+
+    const newLeft = currLeft + imgWidthRem;
+    if (newLeft === 36) {
+        imagesWide.style = `left: ${-36 * 9}rem;`
+    } else {
+        imagesWide.style = `left: ${newLeft}rem;`;
+    }
+    
+    
+    // if (computedLeft === '-36rem') {
+    //     imagesWide.style = 'left:0rem;';
+    // }
+}
+
+rightArrow.addEventListener('click', () => {
+    changeSlideRight();
+})
+leftArrow.addEventListener('click', () => {
+    changeSlideLeft();
+})
