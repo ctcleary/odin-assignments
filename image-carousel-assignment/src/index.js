b44e71c3400cfad18b8fec308e74f115afe79b80
Connor Thomas Cleary <connorthomascleary@gmail.com>
2024-12-26 06:26:56 -0500

    Code wrapped in IIFE, now has looping with transitions.

--- image-carousel-assignment/src/index.js
+++ image-carousel-assignment/src/index.js
@@ -1,71 +1,100 @@
 import "./styles.css";
 
-const imgAssetPath = './img/';
-const imgWidthRem = 36;
-const imagesWide = document.getElementById('images-wide');
 
-// appendImageChild(10);
-
-function appendImageChild(picNum) {
-    const containerDiv = document.createElement('div');
-    containerDiv.classList.add('image-container');
-
-    const img = document.createElement('img');
-    img.classList.add('carousel-image');
-    img.src = `${imgAssetPath}gordon${picNum}.jpg`;
-    img.dataset.picNumber = picNum;
-
-    containerDiv.appendChild(img);
-    imagesWide.appendChild(containerDiv);
-}
-
-for (let i = 1; i <= 10; i++) {
-    appendImageChild(i);
-}
-// appendImageChild(1);
-
-imagesWide.style = 'left:0rem;';
-
-const leftArrow = document.getElementById('arrow-left');
-const rightArrow = document.getElementById('arrow-right');
 
 const carousel = (function() {
+    const imgAssetPath = './img/';
+    const imgWidthRem = 36;
+    const imgTransitionStr = 'left 250ms linear';
+    const imagesWide = document.getElementById('images-wide');
+    
+    const numPictures = 10;
+    
+    // Append the last image first for looping
+    appendImageChild(numPictures);
+    
+    function appendImageChild(picNum) {
+        const containerDiv = document.createElement('div');
+        containerDiv.classList.add('image-container');
+    
+        const img = document.createElement('img');
+        img.classList.add('carousel-image');
+        img.src = `${imgAssetPath}gordon${picNum}.jpg`;
+        img.dataset.picNumber = picNum;
+    
+        containerDiv.appendChild(img);
+        imagesWide.appendChild(containerDiv);
+    }
+    
+    for (let i = 1; i <= numPictures; i++) {
+        appendImageChild(i);
+    }
+    // Append the first image last for looping
+    appendImageChild(1);
+    
+    // Since we have the last img appended first, start at -{imgWidthRem}
+    imagesWide.style.left = `-${imgWidthRem}rem`;
+    
+    // Since we need to remove the transition during looping repositioning
+    imagesWide.style.transition = imgTransitionStr;
+    
+    const leftArrow = document.getElementById('arrow-left');
+    const rightArrow = document.getElementById('arrow-right');
 
+    function changeSlideRight() {
+        const currLeftStr = imagesWide.style.left;
+        const currLeft = parseInt(currLeftStr);
+    
+        const newLeft = currLeft - imgWidthRem;
+        
+        console.log('changeSlideRight newLeft', newLeft);
+        imagesWide.style.left = `${newLeft}rem`;
+    }
+
+    function changeSlideLeft() {
+        const currLeftStr = imagesWide.style.left;
+        const currLeft = parseInt(currLeftStr);
+    
+        const newLeft = currLeft + imgWidthRem;
+
+        console.log('changeSlideLeft newLeft', newLeft);
+        imagesWide.style.left = `${newLeft}rem`;
+    }
+
+    function loopIfShould(e) {
+        const currLeft = parseInt(e.target.style.left, 10);
+        if (currLeft === 0) {
+            console.log('should loop from start-1')
+            imagesWide.style.transition = '';
+            imagesWide.style = `left: ${-imgWidthRem * (numPictures)}rem;`
+            setTimeout(() => {
+                imagesWide.style.transition = imgTransitionStr;
+            }, 0);
+
+        } else if (currLeft === (-imgWidthRem * (numPictures + 1))) {
+            console.log('should loop from end');
+            imagesWide.style.transition = '';
+            imagesWide.style.left = `-${imgWidthRem}em`
+            setTimeout(() => {
+                imagesWide.style.transition = imgTransitionStr;
+            }, 0);
+        }
+    }
+    
+    imagesWide.addEventListener('transitionend', (e) => {
+        loopIfShould(e);
+    })
+
+    rightArrow.addEventListener('click', () => {
+        changeSlideRight();
+    })
+    leftArrow.addEventListener('click', () => {
+        changeSlideLeft();
+    })
+
+    return {
+        changeSlideLeft,
+        changeSlideRight
+    }
 })();
 
-function changeSlideRight() {
-    const currLeftStr = imagesWide.style.left;
-    const currLeft = parseInt(currLeftStr);
-    console.log('currLeft', currLeft);
-
-    const newLeft = currLeft - imgWidthRem;
-    if (newLeft === (-36 * 10)) {
-        imagesWide.style = `left: 0rem;`
-    } else {
-        imagesWide.style = `left: ${newLeft}rem;`;
-    }
-}
-function changeSlideLeft() {
-    const currLeftStr = imagesWide.style.left;
-    const currLeft = parseInt(currLeftStr);
-    console.log('currLeft', currLeft);
-
-    const newLeft = currLeft + imgWidthRem;
-    if (newLeft === 36) {
-        imagesWide.style = `left: ${-36 * 9}rem;`
-    } else {
-        imagesWide.style = `left: ${newLeft}rem;`;
-    }
-    
-    
-    // if (computedLeft === '-36rem') {
-    //     imagesWide.style = 'left:0rem;';
-    // }
-}
-
-rightArrow.addEventListener('click', () => {
-    changeSlideRight();
-})
-leftArrow.addEventListener('click', () => {
-    changeSlideLeft();
-})
