946ff1c712b3aac0e88cf33d48125eba7cea7c82
Connor Thomas Cleary <connorthomascleary@gmail.com>
2024-12-30 16:20:03 -0500

    A number of small improvements to weather app. Can now search for location AND date.

--- weather-app-assignment/src/index.js
+++ weather-app-assignment/src/index.js
@@ -4,32 +4,41 @@ const VISUAL_CROSSING_API_KEY = 'PCGWK76LSAU6S5CDBNM5DCV4M';
 const VISUAL_CROSSING_BASE_REQ = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
 
 
-async function getWeatherData(loc) {
+async function getWeatherData(loc = '', date = null) {
     const locStr = loc || 'manhattan';
-    const today = new Date();
-    const todayStr = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
-    console.log(todayStr);
-    const response = await fetch(`${VISUAL_CROSSING_BASE_REQ}/${locStr}/${todayStr}?key=${VISUAL_CROSSING_API_KEY}`)
-    const responseJSON = await response.json();
+    const searchDate = date ? new Date(date + ' 12:00') : new Date();
+    const dateStr = `${searchDate.getFullYear()}-${searchDate.getMonth() + 1}-${searchDate.getDate()}`
+    
+    const errorMessageEl = document.getElementById('fetch-error');
+    errorMessageEl.textContent = '';
 
-    const addr = responseJSON.resolvedAddress;
-    const day = responseJSON.days[0];
-    console.log(
-        day.tempmax,
-        day.temp,
-        day.tempmin
-    )
-    const dataPackage = {
-        addr: addr,
-        date: day.datetime,
-        temp: day.temp,
-        tempmax: day.tempmax,
-        tempmin: day.tempmin,
-        conditions: day.conditions,
-        description: day.description,
-        precipprob : day.precipprob,
+    try {
+        const response = await fetch(`${VISUAL_CROSSING_BASE_REQ}/${locStr}/${dateStr}?key=${VISUAL_CROSSING_API_KEY}`);
+        if (!response.ok) {
+            throw new Error('Response status :: ', response.status);
+        }
+        const responseJSON = await response.json();
+        
+        const addr = responseJSON.resolvedAddress;
+        const day = responseJSON.days[0];
+
+        const dataPackage = {
+            addr: addr,
+            date: day.datetime,
+            temp: day.temp,
+            tempmax: day.tempmax,
+            tempmin: day.tempmin,
+            conditions: day.conditions,
+            description: day.description,
+            precipprob : day.precipprob,
+        }
+        renderWeatherData(dataPackage);
+
+    } catch (error) {
+        console.error('Error fetching data :: ', error.message)
+        errorMessageEl.textContent = 'Error fetching data. Please provide valid search criteria.';
     }
-    renderWeatherData(dataPackage);
+
 }
 
 getWeatherData();
@@ -49,7 +58,7 @@ function renderWeatherData(data) {
     const temp = document.getElementById('temp');
     const tempMin = document.getElementById('temp-min');
 
-    tempMax.innerHTML = parseAndRound(data.tempmax) + '&deg;';
+    tempMax.innerHTML = parseAndRound(data.tempmax) + '&deg; /';
     temp.innerHTML = parseAndRound(data.temp) + '&deg;';
     tempMin.innerHTML = parseAndRound(data.tempmin) + '&deg;';
 
@@ -62,17 +71,27 @@ function renderWeatherData(data) {
     precipitation.textContent = data.precipprob + '%';
 }
 
-const locSearchForm = document.getElementById('location-search-form');
-const locSearchInput = document.getElementById('search-location')
+const customSearchForm = document.getElementById('custom-search-form');
+const locSearchInput = document.getElementById('search-location');
 
-locSearchForm.addEventListener('submit', (e) => {
+const dateSearchForm = document.getElementById('date-search-form');
+const dateSearchInput = document.getElementById('search-date');
+
+customSearchForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    let hasLoc = true;
     if (!locSearchInput.value || locSearchInput.value === '') {
-        console.log('No search input, exiting.');
-        return;
+        console.log('No location entered.');
+        hasLoc = false;
+    }
+
+    let hasDate = true;
+    if (!dateSearchInput.value || dateSearchInput.value === '') {
+        console.log('No date entered.');
+        hasDate = false;
     }
     
-
-    getWeatherData(locSearchInput.value);
-    locSearchInput.value = '';
+   
+    getWeatherData(locSearchInput.value.trim(), dateSearchInput.value);
 });
